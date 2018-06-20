4478a7be94c59de6690a90488bde95b22da2f2b2
Daniel Varga <varga.daniel@gmx.de>
2018-06-20 22:31:48 +0200

    fix absolute config path

--- src/database/compressor.ts
+++ src/database/compressor.ts
@@ -3,7 +3,7 @@ import {exec} from 'child_process';
 export class Compressor {
   public async compressMatch(match: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-      exec(`7za a "${match}" "./${match}/*"`, (error, stdout) => {
+      exec(`7za a "${match}" "${match}/*"`, (error, stdout) => {
         if (error) {
           reject(error);
           return;
